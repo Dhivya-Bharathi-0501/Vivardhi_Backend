dcd77091732434e5da12c63afa110ceac883ae56
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-10-03 13:48:47 -0700

    added to changes

--- Vivardhi_Backend/Vivardhi_Backend/settings.py
+++ Vivardhi_Backend/Vivardhi_Backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,12 +54,24 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Vivardhi_Backend.urls'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTH_USER_MODEL = 'Authentication.CustomUser'
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+AUTHENTICATION_BACKENDS = [
+    'Authentication.auth.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
 
 TEMPLATES = [
     {
@@ -87,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'manage',
         'USER': 'root',
-        'PASSWORD': 'sys@0805',
+        'PASSWORD': 'Lansub@2024',
         'HOST': 'localhost',
         'PORT': '3306',
     }
