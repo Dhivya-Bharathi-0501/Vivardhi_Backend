c8eb94bd32b939016b1c97e6272aac972c1df356
KeerthiKrish8 <Keerthikak0393@gmail.com>
2025-04-04 17:30:39 +0530

    some changes

--- Vivardhi_Backend/Detailed_Graph/apps.py
+++ Vivardhi_Backend/Detailed_Graph/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DetailedGraphConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Detailed_Graph'
+    name = 'Detailed_Graph1'
